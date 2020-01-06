a46b7d6f0448a575fa11bf77a51bdf7e6c98ee68
Umair Ahmed <umairahmed515@gmail.com>
2020-01-06 12:42:04 +0500

    Modified Event model to include owner field and created link table for attndees in an event

--- simple_event_management_backend/events/admin.py
+++ simple_event_management_backend/events/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from simple_event_management_backend.events.models import Event
+from simple_event_management_backend.events.models import EventAttendance
+
 
 admin.site.register(Event)
+admin.site.register(EventAttendance)
--- simple_event_management_backend/events/models.py
+++ simple_event_management_backend/events/models.py
@@ -3,6 +3,10 @@ from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Event(models.Model):
 
@@ -10,6 +14,16 @@ class Event(models.Model):
     description = models.TextField()
     datetime = models.DateTimeField()
     location_address = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
+class EventAttendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='attendants')
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attending')
+
+    def __str__(self):
+        return "%s - %s" % (self.event, self.attendee)
+    
