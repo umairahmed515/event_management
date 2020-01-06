a8eb667a9cb1f4d11d5dbbca09ff4a742a1f15bf
Umair Ahmed <umairahmed515@gmail.com>
2020-01-06 13:00:41 +0500

    API End point added for getting all events

--- simple_event_management_backend/events/urls.py
+++ simple_event_management_backend/events/urls.py
@@ -1,3 +1,13 @@
 from django.urls import path
 
+from simple_event_management_backend.events.views import (
+    event_get_view
+)
+
 app_name = "events"
+urlpatterns = [
+    #path("create/", view=event_create_view, name="create_ev"),
+    path("get_all/", view=event_get_view, name="get_ev"),
+    #path("update/", view=event_update_view, name="update_ev"),
+    #path("delete/", view=event_delete_view, name="delete_ev")
+]
--- simple_event_management_backend/events/views.py
+++ simple_event_management_backend/events/views.py
@@ -1,1 +1,20 @@
-#Views will go here
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import DetailView, RedirectView, UpdateView
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponse
+from simple_event_management_backend.events.models import Event
+
+
+class EventDetailView(DetailView):
+
+    model = Event
+
+    def get(self, request, *args, **kwargs):
+        queryset = Event.objects.all()
+        event_names = [eve.title for eve in queryset]
+        return HttpResponse(event_names)
+
+event_get_view = EventDetailView.as_view()
